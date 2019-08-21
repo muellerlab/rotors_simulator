5e08cd546b3339c4406eb5819bf349271d63aa3d
nlbucki <nlbucki@gmail.com>
2019-08-21 15:57:39 -0700

    Rewrote controller to be more organized

--- rotors_control/src/nodes/Hiperlab_Controller.cpp
+++ rotors_control/src/nodes/Hiperlab_Controller.cpp
@@ -1,239 +1,111 @@
-/*
- * Hiperlab_Controller.cpp
-
- *
- *  Created on: Feb 10, 2019
- *      Author: ean
- */
-
-#include <ros/ros.h>
-#include <mav_msgs/default_topics.h>
-
 #include "Hiperlab_Controller.hpp"
-
-#include "rotors_control/parameters_ros.h"
 
 namespace rotors_control {
 
 std::mutex cmdRadioChannelMutex;  //protect against concurrency problems
 
-HiperlabController::HiperlabController(
-    const ros::NodeHandle& nh, const ros::NodeHandle& private_nh)
-    : nh_(nh),
-      private_nh_(private_nh) {
-  QuadParams();
+HiperlabController::HiperlabController(ros::NodeHandle& nh, int vehicleId,
+                                       double frequencyLogic,
+                                       double frequencyTelemetry)
+    : _nh(nh),
+      _vehicleId(vehicleId),
+      _subRadioCmd(
+          nh.subscribe("/radio_command" + std::to_string(vehicleId), 1,
+                       &HiperlabController::callbackRadioCmd, this)),
+      _subIMU(
+          nh.subscribe(mav_msgs::default_topics::IMU, 1,
+                       &HiperlabController::callbackImu, this)),
+      _pubMotorVel(
+          nh.advertise<mav_msgs::Actuators>(
+              mav_msgs::default_topics::COMMAND_ACTUATORS, 1)),
+      _pubTelemetry(
+          nh.advertise<hiperlab_rostools::telemetry>(
+              "/telemetry" + std::to_string(vehicleId), 1)),
+      _logic(&_simTimer, 1.0 / frequencyLogic),
+      _lastTelemPubTime(ros::Time::now()),
+      _telemPubPeriod(1.0 / frequencyTelemetry),
+      _lastLogicRunTime(ros::Time::now()),
+      _logicRunPeriod(1.0 / frequencyLogic),
+      _quadType(Onboard::QuadcopterConstants::GetVehicleTypeFromID(vehicleId)),
+      _consts(_quadType) {
 
-  //ROS Subscriber to Radio Command. Define the subscriber parameters and then sub.
-  ros::SubscribeOptions so = ros::SubscribeOptions::create<
-      hiperlab_rostools::radio_command>(
-      "/radio_command" + std::to_string(vehicleId), 1,
-      boost::bind(&HiperlabController::callbackRadioCmd, this, _1),
-      ros::VoidPtr(), &this->rosQueue);
-  this->subRadioCmd = this->nh_.subscribe(so);
+  _logic.Initialise(_quadType, _vehicleId);
 
-  odom_sub = nh_.subscribe(mav_msgs::default_topics::ODOMETRY, 1,
-                           &HiperlabController::Odometry, this);
-
-  //ROS Publisher to telemetry
-  pubTelemetry.reset(
-      new ros::Publisher(
-          nh_.advertise<hiperlab_rostools::telemetry>(
-              "/telemetry" + std::to_string(vehicleId), 1)));
-
-  /*pubMoCap.reset(
-      new ros::Publisher(
-          nh_.advertise < hiperlab_rostools::mocap_output
-   > ("/mocap_output" + std::to_string(vehicleId), 1)));*/
-
-  imu_sub_ = nh_.subscribe(mav_msgs::default_topics::IMU, 1,
-                                &HiperlabController::Run, this);
-
-  motor_velocity_ = nh_.advertise<mav_msgs::Actuators>(
-      mav_msgs::default_topics::COMMAND_ACTUATORS, 1);
-
-  //handle ROS multi-threading
-  this->rosQueueThread = std::thread(
-      std::bind(&HiperlabController::QueueThread, this));
-
-}
-
-void HiperlabController::QuadParams() {
-
-  //create the vehicles
-  Onboard::QuadcopterConstants::QuadcopterType quadcopterType =
-      Onboard::QuadcopterConstants::GetVehicleTypeFromID(vehicleId);
-  Onboard::QuadcopterConstants vehConsts(quadcopterType);
-  _battVoltage = 1.2 * vehConsts.lowBatteryThreshold;
-  _battCurrent = -1.0;
-
-  _IMU_yaw = vehConsts.IMU_yaw;
-  _IMU_pitch = vehConsts.IMU_pitch;
-  _IMU_roll = vehConsts.IMU_roll;
-  _R_inverse =
-      Rotationf::FromEulerYPR(_IMU_yaw, _IMU_pitch, _IMU_roll).Inverse()
-          .GetRotationMatrix();
-
-  _logic.reset(
-      new Onboard::QuadcopterLogic(&simTimer, 1.0 / frequencySimulation));
-  _logic->Initialise(quadcopterType, vehicleId);
-
-  _timerOnboardLogic.reset(new Timer(&simTimer));
-  _onboardLogicPeriod = 1.0 / frequencySimulation;
-
+  // Made up measurements for simulation
+  double battVoltage = 1.2 * _consts.lowBatteryThreshold;
+  double battCurrent = -1.0;
+  double tempMeas = 25;
+  _logic.SetBatteryMeasurement(battVoltage, battCurrent);
+  _logic.SetIMUMeasurementTemperature(tempMeas);
 
   //Initialize the queue to listen to radio commands
-  cmdRadioChannel.queue.reset(
+  double timeDelayOffboardControlLoop = 0.02;
+  _cmdRadioChannel.queue.reset(
       new Simulation::CommunicationsDelay<
           RadioTypes::RadioMessageDecoded::RawMessage>(
-          &simTimer, timeDelayOffboardControlLoop));
-
-  //Initialize Timers
-  Timer cmdRadioTimer(&simTimer);
-  debugTimer.reset(new Timer(&simTimer));
-  timePrintNextInfo = 0;
-
-  t.reset(new Timer(&simTimer));
-
-  //timePublishNextMocap = 0;
-  timePublishNextTelemetry = 0;
-
-}
-
-void HiperlabController::Run(const sensor_msgs::Imu &_imu) {
-
-  _angVel = Vec3d(_imu.angular_velocity.x, _imu.angular_velocity.y,
-                  _imu.angular_velocity.z);
-
-  _accel = Vec3d(_imu.linear_acceleration.x, _imu.linear_acceleration.y,
-                 _imu.linear_acceleration.z);
-
-  _att = Rotationd(_imu.orientation.x, _imu.orientation.y, _imu.orientation.z,
-                   _imu.orientation.w);
-
-  if (_timerOnboardLogic->GetSeconds<double>() > _onboardLogicPeriod) {
-    _timerOnboardLogic->AdjustTimeBySeconds(-_onboardLogicPeriod);
-
-  rateGyroMeas = Vec3f(_angVel);
-  accMeas = Vec3f(_accel);
-
-  _logic->SetBatteryMeasurement(_battVoltage, _battCurrent);
-
-  float const TEMP_MEAS = 25;  //made up temperature for simulation
-  _logic->SetIMUMeasurementTemperature(TEMP_MEAS);
-
-
-  _logic->SetIMUMeasurementRateGyro(rateGyroMeas.x, rateGyroMeas.y,
-                                      rateGyroMeas.z);
-
-    _logic->SetIMUMeasurementAccelerometer(accMeas.x, accMeas.y,
-                                           accMeas.z + 9.81);
-
-  _logic->Run();
-
-  mav_msgs::ActuatorsPtr actuator_msg(new mav_msgs::Actuators);
-
-  actuator_msg->angular_velocities.clear();
-  for (int i = 0; i < 4; i++) {
-    actuator_msg->angular_velocities.push_back(_logic->GetMotorSpeedCmd(i));
-      thrust[i] = (_logic->GetMotorSpeedCmd(i)) * (_logic->GetMotorSpeedCmd(i))
-          * 7.64e-06;
-    }
-
-  motor_velocity_.publish(actuator_msg);
-
-    GetCurrentTelemetry();
-
-  }
-}
-
-void HiperlabController::Odometry(const nav_msgs::Odometry &odom) {
-
-  /*hiperlab_rostools::mocap_output current_mocap;
-   gazebo::math::Pose current_pose;*/
-
- _pos = Vec3d(odom.pose.pose.position.x, odom.pose.pose.position.y,
- odom.pose.pose.position.z);
-
- _vel = Vec3d(odom.twist.twist.linear.x, odom.twist.twist.linear.y,
- odom.twist.twist.linear.z);
-
-  position = Vec3f(_pos);
-  velocity = Vec3f(_vel);
-
-  /*if (t->GetSeconds<double>() > timePublishNextMocap) {
-    timePublishNextMocap += 1 / frequencyMocapOutput;
-
-  current_mocap.vehicleID = vehicleId;
-
-  current_pose.pos.x = odom.pose.pose.position.x;
-  current_pose.pos.y = odom.pose.pose.position.y;
-  current_pose.pos.z = odom.pose.pose.position.z;
-
-  current_pose.rot.w = odom.pose.pose.orientation.w;
-  current_pose.rot.x = odom.pose.pose.orientation.x;
-  current_pose.rot.y = odom.pose.pose.orientation.y;
-  current_pose.rot.z = odom.pose.pose.orientation.z;
-
-  current_mocap.posx = current_pose.pos.x;
-  current_mocap.posy = current_pose.pos.y;
-  current_mocap.posz = current_pose.pos.z;
-
-  current_mocap.attq0 = current_pose.rot.w;
-  current_mocap.attq1 = current_pose.rot.x;
-  current_mocap.attq2 = current_pose.rot.y;
-  current_mocap.attq3 = current_pose.rot.z;
-
-  gazebo::math::Vector3 quatToEuler = current_pose.rot.GetAsEuler();
-  current_mocap.attroll = quatToEuler.x;
-  current_mocap.attpitch = quatToEuler.y;
-  current_mocap.attyaw = quatToEuler.z;
-
-  current_mocap.header.stamp = ros::Time::now();
-
-  pubMoCap->publish(current_mocap);
-   }*/
+          &_simTimer, timeDelayOffboardControlLoop));
 
 }
 
 void HiperlabController::callbackRadioCmd(
-    const hiperlab_rostools::radio_command::ConstPtr &mess) {
-  std::lock_guard < std::mutex > guard(cmdRadioChannelMutex);
+    const hiperlab_rostools::radio_command& radioMsg) {
+  std::lock_guard<std::mutex> guard(cmdRadioChannelMutex);
   //todo: should be a nicer way to do this, using e.g. memcpy...
   RadioTypes::RadioMessageDecoded::RawMessage rawMsg;
   for (int i = 0; i < RadioTypes::RadioMessageDecoded::RAW_PACKET_SIZE; i++) {
-    rawMsg.raw[i] = mess->raw[i];
+    rawMsg.raw[i] = radioMsg.raw[i];
   }
-  cmdRadioChannel.queue->AddMessage(rawMsg);
-  RadioTypes::RadioMessageDecoded msg = RadioTypes::RadioMessageDecoded(
-      rawMsg.raw);
-  _logic->SetRadioMessage(msg);
-  return;
+  _cmdRadioChannel.queue->AddMessage(rawMsg);
 }
 
-void HiperlabController::GetCurrentTelemetry() {
-  if (t->GetSeconds<double>() > timePublishNextTelemetry) {
-    //Telemetry message
-    timePublishNextTelemetry += 1 / frequencyTelemetry;
+void HiperlabController::callbackImu(const sensor_msgs::Imu &imuMsg) {
+  // TODO: Consider adding IMU rotation
+  _logic.SetIMUMeasurementRateGyro(imuMsg.angular_velocity.x,
+                                   imuMsg.angular_velocity.y,
+                                   imuMsg.angular_velocity.z);
+  _logic.SetIMUMeasurementAccelerometer(imuMsg.linear_acceleration.x,
+                                        imuMsg.linear_acceleration.y,
+                                        imuMsg.linear_acceleration.z + 9.81);
+}
 
-  hiperlab_rostools::telemetry telMsgOut;
+void HiperlabController::Run() {
+  ros::Time timeNow = ros::Time::now();
 
-  //Fill out the telemetry package
+  if (_cmdRadioChannel.queue->HaveNewMessage()) {
+    RadioTypes::RadioMessageDecoded msg = RadioTypes::RadioMessageDecoded(
+        _cmdRadioChannel.queue->GetMessage().raw);
+    _logic.SetRadioMessage(msg);
+  }
+
+  if (timeNow - _lastTelemPubTime > _telemPubPeriod) {
+    _lastTelemPubTime = timeNow;
+    PublishTelemetry();
+  }
+
+  if (timeNow - _lastLogicRunTime > _logicRunPeriod) {
+    _lastLogicRunTime = timeNow;
+    RunLogic();
+  }
+}
+
+void HiperlabController::PublishTelemetry() {
   TelemetryPacket::data_packet_t dataPacketRaw1, dataPacketRaw2;
-  _logic->GetTelemetryDataPackets(dataPacketRaw1, dataPacketRaw2);
+  _logic.GetTelemetryDataPackets(dataPacketRaw1, dataPacketRaw2);
 
   TelemetryPacket::TelemetryPacket dataPacket1, dataPacket2;
   TelemetryPacket::DecodeTelemetryPacket(dataPacketRaw1, dataPacket1);
   TelemetryPacket::DecodeTelemetryPacket(dataPacketRaw2, dataPacket2);
 
+  hiperlab_rostools::telemetry telMsgOut;
   telMsgOut.packetNumber = dataPacket1.packetNumber;
   for (int i = 0; i < 3; i++) {
-    telMsgOut.accelerometer[i] = accMeas[i];
-    telMsgOut.rateGyro[i] = rateGyroMeas[i];
-    telMsgOut.position[i] = position[i];
+    telMsgOut.accelerometer[i] = dataPacket1.accel[i];
+    telMsgOut.rateGyro[i] = dataPacket1.gyro[i];
+    telMsgOut.position[i] = dataPacket1.position[i];
   }
 
   for (int i = 0; i < 4; i++) {
-    telMsgOut.motorForces[i] = thrust[i];
+    telMsgOut.motorForces[i] = dataPacket1.motorForces[i];
   }
   telMsgOut.batteryVoltage = dataPacket1.battVoltage;
 
@@ -245,45 +117,63 @@ void HiperlabController::GetCurrentTelemetry() {
       Vec3f(dataPacket2.attitude[0], dataPacket2.attitude[1],
             dataPacket2.attitude[2])).ToEulerYPR();
   for (int i = 0; i < 3; i++) {
-    telMsgOut.velocity[i] = velocity[i];
-    telMsgOut.attitude[i] = _att[i];
+    telMsgOut.velocity[i] = dataPacket2.velocity[i];
+    telMsgOut.attitude[i] = dataPacket2.attitude[i];
     telMsgOut.attitudeYPR[i] = attYPR[i];
-
   }
   telMsgOut.panicReason = dataPacket2.panicReason;
   telMsgOut.warnings = dataPacket2.warnings;
 
   telMsgOut.header.stamp = ros::Time::now();
 
-  pubTelemetry->publish(telMsgOut);
-  }
+  _pubTelemetry.publish(telMsgOut);
 }
 
+void HiperlabController::RunLogic() {
+  _logic.Run();
 
-//Handle ROS multi-threading
-void HiperlabController::QueueThread() {
-  static const double timeout = 0.01;
-  while (this->nh_.ok()) {
-    this->rosQueue.callAvailable(ros::WallDuration(timeout));
+  mav_msgs::ActuatorsPtr actuator_msg(new mav_msgs::Actuators);
+
+  actuator_msg->angular_velocities.clear();
+  for (int i = 0; i < 4; i++) {
+    actuator_msg->angular_velocities.push_back(_logic.GetMotorSpeedCmd(i));
   }
-}
 
+  _pubMotorVel.publish(actuator_msg);
+}
 }
 
 int main(int argc, char** argv) {
+
+  if (argc < 2) {
+    ROS_ERROR("Must specify the vehicle ID");
+    return -1;
+  }
+
+  int const vehicleId = atol(argv[1]);
+  if (vehicleId <= 0 || vehicleId > 255) {
+    ROS_ERROR("ERROR: invalid vehicle ID");
+    return -1;
+  }
+
+  const double frequencyLogic = 500.0;
+  const double frequencyTelemetry = 200.0;
+
   ros::init(argc, argv, "Hiperlab_Controller");
-
   ros::NodeHandle nh;
-  ros::NodeHandle private_nh("~");
-  rotors_control::HiperlabController Hiperlab_Controller(
-      nh, private_nh);
+  rotors_control::HiperlabController Hiperlab_Controller(nh, vehicleId,
+                                                         frequencyLogic,
+                                                         frequencyTelemetry);
+  ros::Rate loop_rate(2 * frequencyLogic);  // Run 2x times faster than we run the onboard logic. Timing will be taken care of in Run() function
+  ros::AsyncSpinner spinner(4);  // TODO: Is 4 threads too many or too few?
+  spinner.start();
 
-  ros::spin();
+  while (ros::ok()) {
+    Hiperlab_Controller.Run();
+    loop_rate.sleep();
+  }
+  spinner.stop();
 
   return 0;
 }
-
-
-
-
 
--- rotors_control/src/nodes/Hiperlab_Controller.hpp
+++ rotors_control/src/nodes/Hiperlab_Controller.hpp
@@ -1,152 +1,66 @@
-/*
- * Hiperlab_Controller.hpp
- *
- *  Created on: Feb 10, 2019
- *      Author: ean
- */
-
-#ifndef SRC_NODES_HIPERLAB_CONTROLLER_HPP_
-#define SRC_NODES_HIPERLAB_CONTROLLER_HPP_
-
-// SYSTEM INCLUDES
 #include <memory>
 #include <mutex>
-#include <thread>
-#include <chrono>
-#include <iostream>
-#include <../stdio.h>
-#include <../boost/bind.hpp>
-#include <random>
 
 #include <mav_msgs/Actuators.h>
-#include <nav_msgs/Odometry.h>
+#include <mav_msgs/default_topics.h>
+#include <sensor_msgs/Imu.h>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
-#include <geometry_msgs/Pose.h>
-#include "ros/callback_queue.h"
 #include "ros/ros.h"
-#include "ros/subscribe_options.h"
-#include <sensor_msgs/Imu.h>
-#include <geometry_msgs/Pose.h>
 #include "rotors_control/common.h"
-#include <gazebo/physics/physics.hh>
+#include "rotors_control/parameters_ros.h"
 
-//=============== Hiperlab ================//
 #include "hiperlab_rostools/radio_command.h"
 #include "hiperlab_rostools/telemetry.h"
-#include "hiperlab_rostools/mocap_output.h"
-
 #include "Common/Math/Vec3.hpp"
 #include "Common/Math/Rotation.hpp"
-
 #include "Components/Logic/QuadcopterLogic.hpp"
-#include "Common/Time/ManualTimer.hpp"
 #include "Common/Time/HardwareTimer.hpp"
 #include "Components/Simulation/CommunicationsDelay.hpp"
-#include "Components/Logic/KalmanFilter6DOF.hpp"
-
 
 namespace rotors_control {
 
 class HiperlabController {
  public:
+
+  HiperlabController(ros::NodeHandle& nh, int vehicleId,
+                     double frequencyLogic, double frequencyTelemetry);
+
+  void callbackRadioCmd(const hiperlab_rostools::radio_command& msg);
+
+  void callbackImu(const sensor_msgs::Imu &imu);
+
+  void Run();
+
+  void PublishTelemetry();
+
+  void RunLogic();
+
+ private:
+  ros::NodeHandle _nh;
+  int const _vehicleId;
+
+  ros::Subscriber _subRadioCmd;
+  ros::Subscriber _subIMU;
+  ros::Publisher _pubMotorVel;
+  ros::Publisher _pubTelemetry;
+
   struct {
     std::shared_ptr<
         Simulation::CommunicationsDelay<
             RadioTypes::RadioMessageDecoded::RawMessage> > queue;
-  } cmdRadioChannel;
-  HiperlabController(const ros::NodeHandle& nh,
-                             const ros::NodeHandle& private_nh);
-  ~HiperlabController() {
+  } _cmdRadioChannel;
 
-  }
+  HardwareTimer _simTimer;
+  Onboard::QuadcopterLogic _logic;
+  ros::Time _lastTelemPubTime;
+  ros::Duration _telemPubPeriod;
+  ros::Time _lastLogicRunTime;
+  ros::Duration _logicRunPeriod;
 
-  void QuadParams();
+  Onboard::QuadcopterConstants::QuadcopterType _quadType;
+  Onboard::QuadcopterConstants _consts;
 
-  void callbackRadioCmd(const hiperlab_rostools::radio_command::ConstPtr &mess);
-
-  void Odometry(const nav_msgs::Odometry &odom);
-
-  void Pose(const geometry_msgs::Pose &pose);
-
-  void Run(const sensor_msgs::Imu &_imu);
-
-  void GetCurrentTelemetry();
-
- private:
-  ros::NodeHandle nh_;
-  ros::NodeHandle private_nh_;
-
-  std::string namespace_;
-  ros::Subscriber imu_sub_;
-  ros::Subscriber odom_sub;
-
-  ros::Publisher motor_velocity_;
-
-  int const vehicleId = 14;
-  int id = vehicleId;
-
-  Onboard::QuadcopterConstants::QuadcopterType quadcopterType;
-  HardwareTimer simTimer;
-  const double frequencySimulation = 500.0;
-  const double frequencyLogic = 500.0;
-  const double frequencyROS = 200;
-  double const timeDelayOffboardControlLoop = 20e-3;
-  const double frequencyMocapOutput = 200;
-  const double frequencyTelemetry = 100;
-  double timePublishROS = 0;
-  double timePublishNextMocap;
-  double timePublishNextTelemetry;
-
-  float _battVoltage, _battCurrent;
-  Vec3d _angVel;
-  Vec3d _accel;
-  Vec3d _pos;
-  Vec3d _vel;
-  Rotationd _att;
-  Vec3f position;
-  Vec3f velocity;
-  Vec3f rateGyroMeas;
-  Vec3f accMeas;
-  float thrust[4];
-
-  float _IMU_yaw;  //[rad]
-  float _IMU_pitch;
-  float _IMU_roll;
-  Matrix<float, 3, 3> _R_inverse;  //rotation matrix correponds to IMU's rotation
-
-  std::default_random_engine _generator;
-  std::normal_distribution<double> _normalDistribution;
-
-  //Noise characteristics:
-  double _stdDevAccNoise, _stdDevRateGyroNoise;
-
-
-  std::shared_ptr<Timer> debugTimer;
-  double timePrintNextInfo;
-  std::shared_ptr<Timer> _timerOnboardLogic;
-  std::shared_ptr<Timer> t;
-  float _onboardLogicPeriod;
-
-  //Radio Messages
-  ros::Subscriber subRadioCmd;
-
-  std::shared_ptr<ros::Publisher> pubTelemetry;
-  std::shared_ptr<ros::Publisher> pubMoCap;
-
-  std::shared_ptr<HiperlabController> v;
-
-  ros::CallbackQueue rosQueue;
-  std::thread rosQueueThread;
-  void QueueThread();
-
-  std::shared_ptr<Onboard::QuadcopterLogic> _logic;
-  std::shared_ptr<Onboard::KalmanFilter6DOF> _kalman;
-
+  //  double const _timeDelayOffboardControlLoop = 20e-3;
 };
 }
-
-
-
-
-#endif /* SRC_NODES_HIPERLAB_CONTROLLER_HPP_ */
